f589a5277c83118f14ea689095c6110b5cfb70f2
Sander <35436862+sndrmn@users.noreply.github.com>
2020-09-15 14:04:46 +1000

    commit

--- azure-aks/variables.tf
+++ azure-aks/variables.tf
@@ -49,9 +49,3 @@ variable "environment" {
   type        = string
   description = "This variable defines the environment to be built"
 }
-# azure region
-variable "location" {
-  type        = string
-  description = "Azure region where the resource group will be created"
-  default     = "north europe"
-}
