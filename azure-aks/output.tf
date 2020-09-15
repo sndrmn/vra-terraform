f9bf0c0bfe9d55ccd8fc939b235e2196a56783c3
Sander <35436862+sndrmn@users.noreply.github.com>
2020-09-15 10:43:58 +1000

    commit

--- azure-aks/output.tf
+++ azure-aks/output.tf
@@ -25,3 +25,13 @@ output "kube_config" {
 output "host" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
 }
+
+output "terraform_state_resource_group_name" {
+  value = azurerm_resource_group.state-rg.name
+}
+output "terraform_state_storage_account" {
+  value = azurerm_storage_account.state-sta.name
+}
+output "terraform_state_storage_container_core" {
+  value = azurerm_storage_container.core-container.name
+}
--- azure-aks/variables.tf
+++ azure-aks/variables.tf
@@ -38,3 +38,20 @@ variable log_analytics_workspace_location {
 variable log_analytics_workspace_sku {
     default = "PerGB2018"
 }
+
+# company
+variable "company" {
+  type        = string
+  description = "This variable defines the name of the company"
+}
+# environment
+variable "environment" {
+  type        = string
+  description = "This variable defines the environment to be built"
+}
+# azure region
+variable "location" {
+  type        = string
+  description = "Azure region where the resource group will be created"
+  default     = "north europe"
+}
