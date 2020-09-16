ade2f8a7fa4504f1989d9e2ee40544884a6630c4
Sander <35436862+sndrmn@users.noreply.github.com>
2020-09-16 10:33:26 +1000

    commit

--- azure-aks/aks-cluster.tf
+++ azure-aks/aks-cluster.tf
@@ -1,29 +1,27 @@
-resource "random_pet" "prefix" {}
-
 provider "azurerm" {
   version = "~> 2.0"
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
-  location = "West US 2"
+  name     = var.clustername
+  location = "Australia East"
 
   tags = {
-    environment = "Demo"
+    environment = var.environment
   }
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
+  name                = var.clustername
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  dns_prefix          = var.clustername
 
   default_node_pool {
     name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_D2_v2"
+    node_count      = var.nodecount
+    vm_size         = var.vmsize
     os_disk_size_gb = 30
   }
 
@@ -37,6 +35,6 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
 
   tags = {
-    environment = "Demo"
+    environment = var.environment
   }
 }
--- azure-aks/variables.tf
+++ azure-aks/variables.tf
@@ -5,3 +5,19 @@ variable "appId" {
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
 }
+
+variable "clustername" {
+  description = "Azure Kubernetes Cluster Name"
+}
+
+variable "nodecount" {
+  description = "AKS Number of Nodes"
+}
+
+variable "vmsize" {
+  description = "AKS Node VMSize"
+}
+
+variable "environment" {
+  description = "Tag for Environment"
+}
