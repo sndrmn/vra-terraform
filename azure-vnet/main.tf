8880dfa31dfcc9a1383463e4f13276d14887b31d
Sander <35436862+sndrmn@users.noreply.github.com>
2020-11-24 18:36:18 +1100

    commit

--- azure-vnet/main.tf
+++ azure-vnet/main.tf
@@ -3,10 +3,14 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "default" {
+  name     = var.rgname
+  location = "Australia East"
+
 resource "azurerm_virtual_network" "main" {
 name                = var.network
-location            = "Australia East"
-resource_group_name = var.rgname
+location            = azurerm_resource_group.default.location
+resource_group_name = azurerm_resource_group.default.name
 address_space       = var.vnet_address
 }
 
