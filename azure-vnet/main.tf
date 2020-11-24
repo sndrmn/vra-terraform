2fa992396e7f4bb326d995d601fa8a7fa0c0fe34
Sander <35436862+sndrmn@users.noreply.github.com>
2020-11-24 20:12:35 +1100

    commit

--- azure-vnet/main.tf
+++ azure-vnet/main.tf
@@ -27,3 +27,4 @@ name                    = var.subnetname2
 resource_group_name     = azurerm_resource_group.default.name
 virtual_network_name    = azurerm_virtual_network.main.name
 address_prefixes        = var.subnet_address2
+}
